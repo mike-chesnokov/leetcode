94f0959ad9c099d36b763ea388ae91f7b9c9b919
mchesnokov <mchesnokov@uma.tech>
2022-04-18 22:02:51 +0300

    + reveresed linked lins II

--- medium_problems/0092_reverse_linked_list_ii.py
+++ medium_problems/0092_reverse_linked_list_ii.py
@@ -10,6 +10,8 @@ Example:
 Input: 1->2->3->4->5->NULL, m = 2, n = 4
 Output: 1->4->3->2->5->NULL
 """
+from typing import  Optional
+
 
 # Definition for singly-linked list.
 class ListNode:
@@ -25,24 +27,27 @@ class ListNode:
 
 
 class Solution(object):
-    def reverseBetween(self, head, m, n):
+    def reverseBetween(self,
+                       head: Optional[ListNode],
+                       left: int,
+                       right: int) -> Optional[ListNode]:
         """
         :type head: ListNode
-        :type m: int
-        :type n: int
+        :type left: int
+        :type right: int
         :rtype: ListNode
         """
         if not head:
             return None
         
-        if m==n:
+        if left == right:
             return head
         
         dummy = ListNode(0)
         dummy.next = head
         
         cur_node = last_node = head
-        if m > 1:
+        if left > 1:
             prev_node = head
         else:
             prev_node = dummy
@@ -50,18 +55,18 @@ class Solution(object):
         temp = None
         m_node = None
         ind = 1
-        while ind < n + 1:
+        while ind < right + 1:
 
-            if ind < m - 1:
+            if ind < left - 1:
                 last_node = last_node.next
                 prev_node = prev_node.next
                 cur_node = cur_node.next
 
-            if ind == m - 1:
+            if ind == left - 1:
                 last_node = last_node.next
                 cur_node = cur_node.next
 
-            if ind == m:
+            if ind == left:
                 last_node = last_node.next
                 next_node = cur_node.next
                 m_node = cur_node
@@ -70,7 +75,7 @@ class Solution(object):
 
                 cur_node = next_node             
 
-            if ind > m and ind <= n:
+            if left < ind <= right:
                 last_node = last_node.next
 
                 next_node = cur_node.next
@@ -85,3 +90,52 @@ class Solution(object):
         m_node.next = last_node
 
         return dummy.next
+
+    def reverse_list(self, head, stop_n):
+        cur_n = head
+        prev_n = None
+
+        while cur_n is not stop_n:
+            next_n = cur_n.next
+            cur_n.next = prev_n
+            prev_n = cur_n
+            cur_n = next_n
+
+        return prev_n, head
+
+    def reverseBetween2(self,
+                       head: Optional[ListNode],
+                       left: int,
+                       right: int) -> Optional[ListNode]:
+        if head is None:
+            return None
+
+        if left == right:
+            return head
+
+        # go to left ind
+        i = 1
+        cur_n = head
+        first_n = None
+        start_reverse_n = None
+        # iterate until right
+        while i < right + 1:
+            if i == left - 1:
+                first_n = cur_n
+            if i == left:
+                start_reverse_n = cur_n
+            cur_n = cur_n.next
+            i += 1
+        last_n = cur_n
+        # inverse inner list
+        new_start_node, new_end_node = self.reverse_list(start_reverse_n,
+                                                         last_n)
+        # process case when left == 1
+        if first_n is None:
+            head = new_start_node
+        else:
+            first_n.next = new_start_node
+        new_end_node.next = last_n
+
+        return head
+
