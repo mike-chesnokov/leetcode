1b48da47ce139dcfdae24dd47d23b1eae73b267d
mchesnokov <mchesnokov@uma.tech>
2022-06-26 13:50:17 +0300

    ! 1 medium

--- array/medium/0078_subsets.py
+++ array/medium/0078_subsets.py
@@ -16,6 +16,40 @@ from itertools import combinations
 
 
 class Solution:
+    def __init__(self):
+        self.result = []
+
+    def backtracking(
+            self,
+            nums: List[int],
+            cur_arr: List[int],  # current array
+            cand_len: int,  # current target length
+            start_ind: int  # current start index
+    ) -> List[List[int]]:
+
+        # print(cur_arr, cand_len, self.result)
+        # base case: len of current array = len of candidates
+        if len(cur_arr) == cand_len:
+            self.result.append(cur_arr.copy())
+            # print('result', self.result)
+            return
+
+            # take next elements from nums
+        for ind in range(start_ind, len(nums)):
+            # add element to current combination
+            cur_arr.append(nums[ind])
+            # go deeper to next elements
+            self.backtracking(nums, cur_arr, cand_len, ind + 1)
+            # backtrack
+            cur_arr.pop()
+
+    def helper_backtrack(self, nums: List[int]) -> List[List[int]]:
+        # iterate over length of candidates
+        for cand_len in range(len(nums) + 1):
+            self.backtracking(nums, [], cand_len, 0)
+
+        return self.result
+
     def built_in_combinations(self, nums: List[int]) -> List[List[int]]:
         """Using built-in method for combinations"""
         res = []
@@ -46,3 +80,9 @@ class Solution:
             res.append([nums[i] for i in range(n) if bitmask[i] == '1'])
 
         return res
+
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        # return self.cascading(nums)
+        # return self.bitmask(nums)
+        # return self.built_in_combinations(nums)
+        return self.helper_backtrack(nums)
