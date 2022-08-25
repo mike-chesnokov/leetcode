e85e4add04f2888d4004436fe58291e5aa519b50
mchesnokov <mchesnokov@uma.tech>
2022-08-25 22:27:12 +0300

    ! easy 100 same tree

--- tree/easy/0100_same_tree.py
+++ tree/easy/0100_same_tree.py
@@ -27,13 +27,65 @@ class TreeNode:
 
 
 class Solution:
-    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+
+    def recursion(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        """
+        Recursive solution
+        """
+        # process case p and q = None
         if p is None and q is None:
             return True
-        if p is None or q is None:
+
+        # process case p of q = None
+        if p is not None and q is None:
+            return False
+        if p is None and q is not None:
             return False
 
+        # process case p != q
         if p.val != q.val:
             return False
-        else:
-            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+        # go recursively to next nodes
+        left_res = self.recursion(p.left, q.left)
+        right_res = self.recursion(p.right, q.right)
+
+        return left_res and right_res
+
+    def are_equal(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+
+        # process case p of q = None
+        if p is not None and q is None:
+            return False
+        if p is None and q is not None:
+            return False
+
+        # process case p != q
+        if p.val != q.val:
+            return False
+
+        return True
+
+    def iteration(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        """
+        Iterative solution
+        """
+        queue = [(p, q)]
+
+        while queue:
+
+            p_, q_ = queue.pop(0)
+
+            if not p_ and not q_:
+                continue
+
+            if not self.are_equal(p_, q_):
+                return False
+            else:
+                queue.append((p_.left, q_.left))
+                queue.append((p_.right, q_.right))
+        return True
+
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        # return self.recursion(p, q)  # Accepted
+        return self.iteration(p, q)  # Accepted
